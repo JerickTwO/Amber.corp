e27ea2ec882c67956f2a9cd1526b6473b54410d7
JerickTwO <137414207+JerickTwO@users.noreply.github.com>
2024-03-17 22:10:44 -0500

    fixed and add components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+			'movil': '	325px',
+			
+			'movil-md':'448px',
+			
+			'tablet': '640px',
+	  
+			'laptop': '1000px',
+
+		  },
 		extend: {},
 	},
 	plugins: [],
